f459b77ffac18e0113b961821403cdfbe5d73bb6
Jan Ibañez <jwgibanez@protonmail.ch>
2021-08-27 13:45:24 +0800

    Initial Android project commit

---
+++ app/src/main/java/io/github/jwgibanez/cartrack/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package io.github.jwgibanez.cartrack.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
